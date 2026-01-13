5b0f388ebd6374e2d0f6e452dbccfd77d75c0da4
shephatia-prog <shephatia1212@gmail.com>
2026-01-13 11:05:03 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -5,3 +5,26 @@ def area_of_circle(radius):
     area_of_circle = (math.pi * radius ** 2)
     
 print(round(area_of_circle,5))
+# Q2: Hollow Right Triangle
+def hollow_right_triangle(n):
+    result = ""
+
+    for i in range(1, n + 1):
+
+        if i == 1:
+            result = "*" + "\n"
+
+        elif i == 2:
+            result = "**" + "\n"
+
+        elif i == n: 
+            result = "*" * n + "\n"
+
+        else: 
+            result += "*" + " " * (i - 2) + "*" + "\n"
+
+    return result 
+
+        
+    
+print(hollow_right_triangle(5))
